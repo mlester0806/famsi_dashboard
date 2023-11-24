b4def1028aac45130fa98f4194f0f1e3e3e528d3
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-24 13:01:20 +0800

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->boolean('user_type')->default(false); // 0=>Applicant, 1=>Hr Staff, 2=>Hr Admin, 3=>Admin
+            $table->tinyInteger('user_type'); // 0=>Applicant, 1=>Hr Staff, 2=>Hr Admin, 3=>Admin
             $table->boolean('is_active');
             $table->timestamps();
         });
