499da4d5d725f8dd2dbf9ccfeeb61017f9307a89
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-11-30 09:31:17 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://famsi-web-a6pqk.ondigitalocean.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://famsi-web-a6pqk.ondigitalocean.app', 'https://famsi-web.site'],
 
     'allowed_origins_patterns' => [],
 
