29d7117ccc8b0af141f77c16a609a0625f1808f3
mlester0806 <114319938+mlester0806@users.noreply.github.com>
2023-12-04 11:46:10 +0800

    Update TableDisqualified.vue

--- resources/js/Components/TableDisqualified.vue
+++ resources/js/Components/TableDisqualified.vue
@@ -451,7 +451,7 @@ watch(
 
                             <tr v-if="roles.data.length === 0">
                                 <td
-                                    colspan="10"
+                                    colspan="11"
                                     class="max-w-sm text-center p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400"
                                 >
                                     <div
